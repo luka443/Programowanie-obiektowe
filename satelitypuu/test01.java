f74036d656ebe0580e553d956975aafc6bd24122
Łukasz <109036862+luka443@users.noreply.github.com>
2023-01-12 01:05:32 +0100

    Add files via upload

--- satelitypuu/test01.java
+++ satelitypuu/test01.java
@@ -3,6 +3,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class test01 {
     private final static String url = "https://www.satbeams.com/satellites";
@@ -12,13 +13,18 @@ public class test01 {
         Document doc = Jsoup.connect(url).get();
         Elements media = doc.select("table[cellpadding=0][id=sat_grid]");
         Elements tmp = media.first().select("TR");
-        String[ ][ ] arr = new String[tmp.size()][10];
+        String[ ]arr = new String[tmp.size()];
+        ArrayList<String> elemn = new ArrayList<String>();
         for (Element el: tmp) {
 
             if(!el.children().text().contains("deorbited") && !el.children().text().contains("retired")
                     && !el.children().text().contains("failed")) {
 
-                arr =el.children().text();
+                System.out.println(el.children().text());
+                elemn.add(el.children().text());
+
+                elemn.add("/n");
+
             }
 
         }
